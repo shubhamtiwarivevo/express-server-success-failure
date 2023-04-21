8af3b6238ea50bd3f4c30b1baef2b558513e61b0
shubhamtiwarivevo <76911029+shubhamtiwarivevo@users.noreply.github.com>
2023-04-21 15:30:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# express-server-success-failure
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/express-simple-anvoxd)
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3010;
+const path = require('path');
+const cors = require('cors');
+app.use(cors());
+function getRandomItemFromArray(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+app.get('/', (req, res) => {
+  const status = getRandomItemFromArray([200, 400]);
+  if (status === 200) {
+    return res.status(status).json({ message: 'ok' });
+  }
+  if (status === 400) {
+    return res.status(status).json({ message: 'failed' });
+  }
+});
+// app.use(express.static('static'));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve('pages/index.html'));
+// });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
