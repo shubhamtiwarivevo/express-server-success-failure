795671e98fc75521b37583c64aaa9cbe1c86c44c
shubhamtiwarivevo <76911029+shubhamtiwarivevo@users.noreply.github.com>
2023-04-21 16:10:34 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,17 +4,26 @@ const port = process.env.PORT || 3001;
 const path = require('path');
 const cors = require('cors');
 app.use(cors());
+const delay = (ms) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      res();
+    }, ms);
+  });
+};
+
 function getRandomItemFromArray(array) {
   const randomIndex = Math.floor(Math.random() * array.length);
   return array[randomIndex];
 }
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   const status = getRandomItemFromArray([200, 400]);
   if (status === 200) {
     return res.status(status).json({ message: 'ok' });
   }
   if (status === 400) {
+    await delay(4000);
     return res.status(status).json({ message: 'failed' });
   }
 });
